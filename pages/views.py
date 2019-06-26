9ae1911c9032a0eceac8889a1ca83eaecfb1f584
surajkarki66 <suraj.karki500@gmail.com>
2019-06-26 18:23:58 +0545

    latest

--- pages/views.py
+++ pages/views.py
@@ -5,4 +5,27 @@ class HomePageView(TemplateView):
 
     template_name = 'users/home.html'
 
+    def index(request):
+    if request.method == 'POST':
+        firstname = request.POST.get('fname')
+        lastname = request.POST.get('lname')
+
+        r = requests.get('http://api.icndb.com/jokes/random?firstName=' + firstname + '&lastName=' + lastname)
+        json_data = json.loads(r.text)
+        joke = json_data.get('value').get('joke')
+
+        context = {'joker': joke}
+        return render(request, 'users/index.html', context)
+    else:
+        firstname = 'Suraj'
+        lastname = 'Karki'
+
+        r = requests.get('http://api.icndb.com/jokes/random?firstName=' + firstname + '&lastName=' + lastname)
+        json_data = json.loads(r.text)
+        joke = json_data.get('value').get('joke')
+
+        context = {'joker': joke}
+        return render(request, 'users/index.html', context)
+
+
 
